3bd260159f88ed490bbaa3f0f6d6ac8840d33556
AlexLeoChumak <chumak1106@gmail.com>
2023-11-20 19:34:04 +0300

    create controller createUserDto

--- server/src/users/users.controller.ts
+++ server/src/users/users.controller.ts
@@ -1,4 +1,25 @@
-import { Controller } from '@nestjs/common';
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import {
+  Body,
+  Controller,
+  Header,
+  HttpCode,
+  HttpStatus,
+  Post,
+} from '@nestjs/common';
+import { UsersService } from './users.service';
+import { CreateUserDto } from './dto/create-user.dto';
 
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+  constructor(private readonly userService: UsersService) {}
+
+  @Post('/signup')
+  @HttpCode(HttpStatus.CREATED)
+  @Header('Content-type', 'application/json')
+  createUser(@Body() CreateUserDto: CreateUserDto) {
+    return this.userService.create(CreateUserDto);
+  }
+}
+
+//38:19
